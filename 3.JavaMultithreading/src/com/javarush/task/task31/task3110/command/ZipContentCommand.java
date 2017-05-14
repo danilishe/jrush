19fd8a55a957fc46e2a0a7bc46d3a2942d7f6f45
danilishe <suetin.danil@gmail.com>
2017-05-14 10:58:19 +0400

    решение с javarush

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipContentCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipContentCommand.java
@@ -4,26 +4,22 @@ import com.javarush.task.task31.task3110.ConsoleHelper;
 import com.javarush.task.task31.task3110.FileProperties;
 import com.javarush.task.task31.task3110.ZipFileManager;
 
-/**
- * Created by Данил on 09.05.2017.
- *
- * 1. Выведи сообщение «Просмотр содержимого архива.»
- 2. Создай объект класса ZipFileManager с помощью метода getZipFileManager()
- 3. Выведи сообщение «Содержимое архива:»
- 4. Получи список файлов архива с помощью метода getFilesList()
- 5. Выведи свойства каждого файла в консоль. Тут нам и пригодится ранее реализованный метод toString() класса FileProperties
- 6. Выведи сообщение «Содержимое архива прочитано.»
- 7. Запусти программу и проверь, что команда «просмотреть содержимое архива» работает
- */
+import java.util.List;
+
 public class ZipContentCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
         ConsoleHelper.writeMessage("Просмотр содержимого архива.");
+
         ZipFileManager zipFileManager = getZipFileManager();
-        for (FileProperties fileProps :
-                zipFileManager.getFilesList()) {
-            ConsoleHelper.writeMessage(fileProps.toString());
+
+        ConsoleHelper.writeMessage("Содержимое архива:");
+
+        List<FileProperties> files = zipFileManager.getFilesList();
+        for (FileProperties file : files) {
+            ConsoleHelper.writeMessage(file.toString());
         }
+
         ConsoleHelper.writeMessage("Содержимое архива прочитано.");
     }
 }
