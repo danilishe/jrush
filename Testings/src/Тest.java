f2aa825b5b5b54c67c311ae60b1c0f5394db37f2
danilishe <suetin.danil@gmail.com>
2017-05-15 17:44:30 +0400

    проверочный модуль

--- Testings/src/Тest.java
+++ Testings/src/Тest.java
@@ -1,10 +1,25 @@
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.zip.ZipEntry;
 
+import com.javarush.task.task31.task3110.*;
 /**
  * Created by Данил on 26.02.2017.
  */
 public class Тest {
-    public static void main (String[] args) {
-        String name = null;
+
+
+    public static void main (String[] args) throws Exception {
+        ZipFileManager zipFileManager = new ZipFileManager(Paths.get("c:/tmp/test2.zip"));
+List<Path> filesForDelete = new ArrayList<>();
+filesForDelete.add(Paths.get("temp/3.txt"));
+filesForDelete.add(Paths.get("temp/5.txt"));
+zipFileManager.removeFiles(filesForDelete);
+
     }
 }
