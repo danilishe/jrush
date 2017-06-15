b8bb9955be5e1781326673ac4783536940b75648
danilishe <suetin.danil@gmail.com>
2017-06-15 23:23:26 +0400

    Решил на основе чужого варианта

--- 4.JavaCollections/src/com/javarush/task/task31/task3106/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3106/Solution.java
@@ -2,24 +2,20 @@ package com.javarush.task.task31.task3106;
 
 import java.io.*;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 /*
 Разархивируем файл
 */
 public class Solution {
-    public static void main(String[] args) {
-        byte[] buffer = new byte[8024];
-        int len;
+    public static void main(String[] args) throws IOException {
 
-/*        args = new String[] {
+/*        args = new String[]{
                 "c:/tmp/some.mp3",
                 "c:/tmp/test.003",
                 "c:/tmp/test.002",
@@ -30,35 +26,24 @@ public class Solution {
         };*/
 
         String resultFileName = args[0];
-        List<String> fileNamePart = new ArrayList<>();
-        fileNamePart.addAll(Arrays.asList(Arrays.copyOfRange(args, 1, args.length - 1)));
-        Collections.sort(fileNamePart);
 
-        OutputStream outputStream = null;
-        try {
-            Path temFile = Files.createTempFile(null, null);
-            outputStream = Files.newOutputStream(temFile);
-            for (String part :
-                    fileNamePart) {
-                InputStream inputStream = Files.newInputStream(Paths.get(part));
+        Arrays.sort(args = Arrays.copyOfRange(args, 1, args.length));
+        List zipParts = new ArrayList<>();
+        for (String partName : args) {
+            zipParts.add(new FileInputStream(partName));
+        }
 
-                while((len = inputStream.read(buffer)) > 0) {
-                    outputStream.write(buffer, 0, len);
-                }
-                inputStream.close();
+        byte[] buffer = new byte[1024];
+        int readByteCnt;
+
+        try (
+             ZipInputStream zipIn = new ZipInputStream(new SequenceInputStream(Collections.enumeration(zipParts)));
+             FileOutputStream fileOut = new FileOutputStream(new File(resultFileName))) {
+            zipIn.getNextEntry();
+
+            while ((readByteCnt = zipIn.read(buffer)) > 0) {
+                fileOut.write(buffer, 0, readByteCnt);
             }
-            outputStream.close();
-
-            ZipInputStream zipInputStream = new ZipInputStream(Files.newInputStream(temFile));
-            OutputStream newFile = Files.newOutputStream(Paths.get(resultFileName));
-            ZipEntry zipEntry = zipInputStream.getNextEntry();
-            while ( (len = zipInputStream.read(buffer)) > 0) {
-                newFile.write(buffer, 0, len);
-            }
-            zipInputStream.closeEntry();
-            zipInputStream.close();
-            newFile.close();
-
-        } catch (Exception ignore) {}
+        }
     }
 }
